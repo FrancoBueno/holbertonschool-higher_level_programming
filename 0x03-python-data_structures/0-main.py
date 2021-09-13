dcd914035b57c3b436e5db0335353a3ce7920bdd
FrancoBuenoHolberton <3290@holbertonschool.com>
2021-09-13 15:57:30 +0000

    fix

---
+++ 0x03-python-data_structures/0-main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+print_list_integer = __import__('0-print_list_integer').print_list_integer
+
+my_list = [1, 2, 3, 4, 5]
+print_list_integer(my_list)
+
+guillaume@ubuntu:~/0x03$ ./0-main.py
+1
+2
+3
+4
+5
