9fe47843d7362505f9b4aeb647b4fa592bde6bdb
FrancoBuenoHolberton <3290@holbertonschool.com>
2021-09-13 23:44:13 +0000

    fin9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list is None or len(my_list) < 1:
-        return 
+    if len(my_list) < 1:
+        return None
         maxx = my_list[0]
     for i in my_list:
-        if (maxx < i):
+        if (i > maxx):
             maxx = i
         return maxx
