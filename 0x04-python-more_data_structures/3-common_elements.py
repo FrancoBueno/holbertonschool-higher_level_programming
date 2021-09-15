ec808a8425d2f1ca799d710ff34d79acdbf9b6dd
FrancoBuenoHolberton <3290@holbertonschool.com>
2021-09-15 17:02:44 +0000

    fin 3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    listt = []
     for i in set_1:
         for f in set_2:
             if i == f:
-                listt = i
-                return listt
+                return i
