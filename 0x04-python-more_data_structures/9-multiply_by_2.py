f13872f717939f980b5b34f0bcfb2bab61bab055
FrancoBuenoHolberton <3290@holbertonschool.com>
2021-09-15 18:30:41 +0000

    nine

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    listnew = []
+    listnew = a_dictionary
+    for i in listnew:
+        listnew[i] = a_dictionary[i] * 2
+    return listnew
