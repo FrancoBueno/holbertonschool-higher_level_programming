ca9c0e87871d352200c0021cee0dc48e7f8be3c1
FrancoBuenoHolberton <3290@holbertonschool.com>
2021-09-15 17:05:32 +0000

    3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    for i in set_1:
-        for f in set_2:
-            if i == f:
-                return i
+    return(set_1.intersection(set_2))
