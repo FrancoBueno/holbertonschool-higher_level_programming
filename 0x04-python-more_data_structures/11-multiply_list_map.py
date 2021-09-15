d986480758fad21bfcdd63bee701b343ac332c76
FrancoBuenoHolberton <3290@holbertonschool.com>
2021-09-15 18:48:49 +0000

    111

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    listnew = []
-    listnew = list(map(lambda x: x * number, my_list))
-    return listnew
+return list(map(lambda x: x * number, my_list))
+    
