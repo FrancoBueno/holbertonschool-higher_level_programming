3d0c1e3fbd8168403596c74f8d9b9a10ce218138
FrancoBuenoHolberton <3290@holbertonschool.com>
2021-09-15 14:33:15 +0000

    nueva nueva

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,5 @@
 #!/bin/usr/python3
 def search_replace(my_list, search, replace):
-    listeana = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
-    for i in listeana:
-        if listeana[i] == 2:
-            listeana.insert(i, 89)
-        return listeana
+    listup = []
+    listup = list(map(lambda a: replace if a is search else a, my_list))
+    return listup
