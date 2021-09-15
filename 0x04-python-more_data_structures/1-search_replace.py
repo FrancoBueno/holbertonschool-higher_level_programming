1ee3afd6d4b73d94c716159dd2b0f863c78ea34f
FrancoBuenoHolberton <3290@holbertonschool.com>
2021-09-15 16:47:27 +0000

    ahora

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,3 @@
 #!/bin/usr/python3
 def search_replace(my_list, search, replace):
-    listup = []
-    listup = list(map(lambda a: replace if a is search else a, my_list))
-    return listup
+    return (list(map(lambda x: replace if x is search else x, my_list)))
