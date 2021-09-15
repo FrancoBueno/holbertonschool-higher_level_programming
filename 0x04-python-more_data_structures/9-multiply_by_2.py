be9bb3d9d41b38e94313cd3039768c85ee27a4e9
FrancoBuenoHolberton <3290@holbertonschool.com>
2021-09-15 18:33:08 +0000

    aberf

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     listnew = []
-    listnew = a_dictionary
+    listnew = dict(a_dictionary)
     for i in listnew:
         listnew[i] = a_dictionary[i] * 2
     return listnew
