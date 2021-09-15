3112b6bd7b38a05b52989865edbdf59d8a69ecf3
FrancoBuenoHolberton <3290@holbertonschool.com>
2021-09-15 17:24:10 +0000

    fix1

--- 0x04-python-more_data_structures/1-main.py
+++ 0x04-python-more_data_structures/1-main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 search_replace = __import__('1-search_replace').search_replace
 
 my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
@@ -6,3 +7,4 @@ new_list = search_replace(my_list, 2, 89)
 
 print(new_list)
 print(my_list)
+
