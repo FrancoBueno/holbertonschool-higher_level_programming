8c3a93422e1f585b1c394f0ce9664e7d6223864a
FrancoBuenoHolberton <3290@holbertonschool.com>
2021-09-27 15:54:58 +0000

    fix2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     contador = 0
-    for f in range (x):
+    for f in range(x):
         try:
             print("{:d}".format(my_list[f]), end="")
             contador += 1
