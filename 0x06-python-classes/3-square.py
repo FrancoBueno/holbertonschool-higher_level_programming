81f48c6361bc9fd8b3021fa44ccce2574fcaae3f
FrancoBuenoHolberton <3290@holbertonschool.com>
2021-09-28 15:58:32 +0000

    3square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+            pass
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+            pass
+        else:
+            self.__size = size
+    def area(self):
+        return ((self.__size)** 2)
