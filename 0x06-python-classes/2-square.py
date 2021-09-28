e52ec9ee5902c9f55b3b63f96e40e550eed907cc
FrancoBuenoHolberton <3290@holbertonschool.com>
2021-09-28 22:23:00 +0000

    2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 "intrapoint"
+
+
 class Square:
-    "square square"
-    def __init__(self,size=0):
+    """square square"""
+    def __init__(self, size=0):
         if type(size) is not int:
             raise TypeError("size must be an integer")
             pass
