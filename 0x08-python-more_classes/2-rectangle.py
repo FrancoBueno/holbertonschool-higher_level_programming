c1e7389f44dcd8e925ead2afd20f1f1de21db20b
FrancoBuenoHolberton <3290@holbertonschool.com>
2021-10-04 21:21:51 +0000

    fix

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -55,4 +55,5 @@ class Rectangle:
         """asdasdasd"""
         if self.__width == 0 or self.__height == 0:
             return 0
-        return (self.__width * 2) + (self.__height * 2)
+        else:
+            return (self.__width * 2) + (self.__height * 2)
