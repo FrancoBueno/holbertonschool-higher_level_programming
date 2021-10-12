1daf4f41a33a31b94d66ba60502b35b5e9692c45
root <3290@holbertonschool.com>
2021-10-12 10:22:49 -0700

    0x0A-python-inheritance

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""Raise exception based in the 5 excersice and area"""
+
 
 class BaseGeometry:
+    """Create instance """
     def area(self):
             raise Exception("area() is not implemented")
