da5f6f7f8031805e6b622e86f2de999a89ea993d
root <3290@holbertonschool.com>
2021-10-12 11:44:54 -0700

    0x0A-python-inheritance

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Raise exception based in the 5 excersice and area"""
+
+
+class BaseGeometry:
+    """Create instance """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        self.value = value
+        self.name = name
+        if isinstance(self.value, int) is False:
+            raise TypeError("{} must be an integer".format(self.name))
+        if self.value <= 0:
+            raise ValueError("{} must be greater than 0".format(self.name))
+
+class Rectangle(BaseGeometry):
+
+    def __init__(self, width, height):
+        self.integer_validator("height", height)
+        self.integer_validator("width", width)
+        self.__width = width
+        self.__height = height
