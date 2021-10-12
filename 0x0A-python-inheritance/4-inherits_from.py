568b399073bf631a870697acc563c79fc37eb2c0
root <3290@holbertonschool.com>
2021-10-12 10:09:30 -0700

    0x0A-python-inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,7 +2,7 @@
 """ functiont that verifi si es una subclasse"""
 def inherits_from(obj, a_class):
     """functiont that verify is subclass"""
-    if a_class is bool:
-        return()
+    if a_class is False:
+        return(False)
     else:
-        return issubclass(type(obj), a_class)
+        return issubclass(type(obj), a_class) and type(obj) is not a_class
