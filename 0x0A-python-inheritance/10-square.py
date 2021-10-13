f351a8afe46eed24819bce88a624a45a1951d302
root <3290@holbertonschool.com>
2021-10-13 15:16:55 -0700

    0x0A-python-inheritance

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -2,12 +2,13 @@
 """Raise exception based in the 5 excersice and area"""
 Rectangle = __import__("9-rectangle").Rectangle
 
+
 class Square(Rectangle):
     """New Class Square That Print The size """
     def __init__(self, size):
         try:
             self.integer_validator("size", size)
             super().__init__(size, size)
+
         except Exception as e:
             raise e
-
