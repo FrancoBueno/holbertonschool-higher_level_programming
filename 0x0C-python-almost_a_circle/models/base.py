13f48f2375fadd9b9acc0c2500a84efc2e67aea3
root <3290@holbertonschool.com>
2021-10-14 10:40:12 -0700

    fix

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Class Base """
+
+
+class Base:
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """ Init constructor """
+        if id is not None:
+            id = Idnew
+
