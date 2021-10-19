65db1b0c62e75c764a1eef76f813a40acb0ab0ee
root <3290@holbertonschool.com>
2021-10-19 05:50:36 -0700

    0x0C-python-almost_a_circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -8,14 +8,14 @@ class Base:
 
     def __init__(self, id=None):
         """ Init constructor """
-        if id not None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
 
     def to_json_string(list_dictionaries):
-            if list_dictionaries == 0 or list_dictionaries == None:
-                return ("[]")
-            else:
-                return json.dumps(list_dictionaries)
+        if list_dictionaries == 0 or list_dictionaries == None:
+            return ("[]")
+        else:
+            return json.dumps(list_dictionaries)
