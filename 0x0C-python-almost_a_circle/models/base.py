704932eb730c52c91a1c0ae824149ef798ef71a0
root <3290@holbertonschool.com>
2021-10-19 05:45:38 -0700

    0x0C-python-almost_a_circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -8,7 +8,7 @@ class Base:
 
     def __init__(self, id=None):
         """ Init constructor """
-        if id is not None:
+        if id not None:
             self.id = id
         else:
             Base.__nb_objects += 1
