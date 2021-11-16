c6b3749df14fe9e4d92fbb0f9a1ebcdb1821954a
root <3290@holbertonschool.com>
2021-11-16 11:07:55 -0800

    9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,7 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table (id, name, score) VALUE (1, 'Jhon', 10);
-INSERT INTO second_table (id, name, score) VALUE (2, 'Alex', 3);
-INSERT INTO second_table (id, name, score) VALUE (3, 'Bob', 14);
-INSERT INTO second_table (id, name, score) VALUE (4, 'George', 8);
+INSERT INTO second_table (id, name, score) VALUES (1, 'Jhon', 10);
+INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
+INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
