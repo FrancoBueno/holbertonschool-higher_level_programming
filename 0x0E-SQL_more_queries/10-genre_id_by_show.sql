3f7011095dfd136b8cf0513b06408f6910b7a3d1
root <3290@holbertonschool.com>
2021-11-17 13:29:21 -0800

    aver

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -3,5 +3,5 @@
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROm tv_shows
 INNER JOIN tv_show_genres
-ON tv_shows.id=tv_show_genres.genre_id
+ON tv_shows.id=tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
