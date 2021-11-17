22192fc0b4cd2b8a773b111ecfffffd8c124c475
root <3290@holbertonschool.com>
2021-11-17 08:52:48 -0800

    Fix - NOT NULL

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,3 +1,3 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
-CREATE TABLE IF NOT EXISTS force_name (id INT,name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS force_name (id INT,name VARCHAR(256)NOT NULL);
