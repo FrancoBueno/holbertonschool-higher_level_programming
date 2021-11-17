892cc5ded1713b36261fbe6059f9032feef68453
root <3290@holbertonschool.com>
2021-11-17 08:07:03 -0800

    1- Create User and Grant Permissions

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is the best!
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
+
