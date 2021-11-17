62ebcf323bb85ffb66707831101fc6badcd862bf
root <3290@holbertonschool.com>
2021-11-17 09:12:07 -0800

    fin5?

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,3 +1,3 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
-CREATE TABLE IF NOT EXISTS unique_id(id INT UNIQUE, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS unique_id(id INT UNIQUE DEFAULT 1, name VARCHAR(256) NOT NULL);
