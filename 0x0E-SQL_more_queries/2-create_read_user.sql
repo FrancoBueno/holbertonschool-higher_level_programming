84c09fb3a062530dee65fb5e8f3c237f9d60fde4
root <3290@holbertonschool.com>
2021-11-17 08:22:12 -0800

    2- create database and user in localhost

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,3 +2,4 @@
 -- because Batch 3 is the best!
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'hbtn_0d_2' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON `hbtn_0d_2`.* TO `user_0d_2`@`localhost`;
