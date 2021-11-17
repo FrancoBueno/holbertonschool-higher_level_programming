48dd9c71a91bd1a9a45b0068d29a51d8575ac33c
root <3290@holbertonschool.com>
2021-11-17 09:23:11 -0800

    fin6- DB + TABLE + uniqID + AUTOGNRTD

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is the best!
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256) NOT NULL);
+
