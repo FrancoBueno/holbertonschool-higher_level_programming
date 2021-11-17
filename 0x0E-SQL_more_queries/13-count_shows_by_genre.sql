1cd0c424df2d35b4cf383283157683994b11160c
root <3290@holbertonschool.com>
2021-11-17 14:17:17 -0800

    fin13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,8 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
-SELECT tv_genres.name AS genre, COUNT(*) AS number_shows
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres
-INNER JOIN tv_show_genres
-ON tv_genres.id=tv_show_genres.genre_id
+JOIN tv_show_genres ON tv_show_genres.genre_id=tv_genres.id
+WHERE tv_show_genres.genre_id IS NOT NULL
 GROUP BY tv_show_genres.genre_id
-ORDER BY number_shows DESC;
+ORDER BY number_of_shows DESC;
