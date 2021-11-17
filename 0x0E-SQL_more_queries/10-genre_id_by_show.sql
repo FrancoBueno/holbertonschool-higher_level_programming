9f1bc94a5a3b0e54697d1249179bacca362f159f
root <3290@holbertonschool.com>
2021-11-17 13:21:26 -0800

    10fin

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is the best!
+SELECT tv_shows.tittle, tv_show_genres.genre_id
+FROm tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id=tv_show_genres.genre_id
+ORDER BY tv_shows.tittle, tv_show_genres.genre.id;
