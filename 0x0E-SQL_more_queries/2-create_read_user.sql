55964df31fa1f525f54a10958b89dd9cd071e97a
root <3290@holbertonschool.com>
2021-11-17 08:14:51 -0800

    2- create database and user in localhost

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is the best!
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
