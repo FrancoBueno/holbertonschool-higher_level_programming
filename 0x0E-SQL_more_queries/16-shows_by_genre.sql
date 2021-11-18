e80121b5c77549a09ca6ad426db945d46759b4ce
root <3290@holbertonschool.com>
2021-11-17 16:48:39 -0800

    16 FIX

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -2,7 +2,7 @@
 -- because Batch 3 is the best!
 SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
-INNER JOIN tv_show_genres
+LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres
 ON tv_show_genres.genre_id=tv_genres.id
