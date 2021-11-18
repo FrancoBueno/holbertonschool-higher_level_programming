2975e5393ff899b501239c55f880a8767bf0cb46
root <3290@holbertonschool.com>
2021-11-17 16:47:56 -0800

    16 FIX

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -6,5 +6,4 @@ INNER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres
 ON tv_show_genres.genre_id=tv_genres.id
-WHERE tv_genres.name= "Comedy"
 ORDER BY tv_shows.title, tv_genres.name;
