282ebb76002c1e0c63ed93e0c0bd45f6e44ec1cf
root <3290@holbertonschool.com>
2021-11-17 16:46:16 -0800

    finproject

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,10 @@
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is the best!
+SELECT tv_shows.title, tv_genres.name
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres
+ON tv_show_genres.genre_id=tv_genres.id
+WHERE tv_genres.name= "Comedy"
+ORDER BY tv_shows.title, tv_genres.name;
