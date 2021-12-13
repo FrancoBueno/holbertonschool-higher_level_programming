e6449d88d6fb6262622d0ffdcfc12d498b20b8cc
root <3290@holbertonschool.com>
2021-12-13 09:58:08 -0800

    10fn

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,7 +3,7 @@
 const Arguments = process.argv;
 const Alfredo = parseInt(Arguments['2']);
 function factorial (Alfredo) {
-  if (Alfredo === 0) {
+if (Alfredo === 0 || Number.isNaN(Alfredo)) {
     return 1;
   }
   return Alfredo * factorial(Alfredo - 1);
