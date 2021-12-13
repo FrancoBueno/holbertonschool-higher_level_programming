97ad648d3e50d4acefac458a9e439db378901763
root <3290@holbertonschool.com>
2021-12-13 07:07:08 -0800

    fin3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // args in Javascript
 const Arguments = process.argv;
-if (Arguments['2'] == undefined) {
+if (Arguments['2'] === undefined) {
   console.log('No argument');
 }
 if (Arguments['2']) {
