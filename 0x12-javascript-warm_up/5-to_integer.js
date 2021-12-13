7e69d4b2d757b858371458a4ca7fa2739cd261ac
root <3290@holbertonschool.com>
2021-12-13 08:03:56 -0800

    fin5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
 // Integrers Number
-const Arguments = process.argv
+const Arguments = process.argv;
 const integral = parseInt(Arguments['2']);
-if (!integral){
-console.log('Not a number');
-}
-else 
-console.log('My number: ' + integral);
-
+if (!integral) {
+  console.log('Not a number');
+} else { console.log('My number: ' + integral); }
