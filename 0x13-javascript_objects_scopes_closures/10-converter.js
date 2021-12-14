2696096b078f4abf4db3cf082ed996976fcd4907
root <3290@holbertonschool.com>
2021-12-14 09:19:47 -0800

    fin10

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 exports.converter = function (base) {
-  let ready = [];
   function convertidor (c) {
-    ready = c.toString(base);
-    return ready;
+    return c.toString(base);
   }
   return convertidor;
 };
