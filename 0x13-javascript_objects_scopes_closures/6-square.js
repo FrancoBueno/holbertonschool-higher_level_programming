8cc5c3acc9f34a3f5c61d15585e7f0035cdd8313
root <3290@holbertonschool.com>
2021-12-14 08:42:01 -0800

    semi?

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,15 +2,13 @@
 
 const newSquare = require('./5-square');
 module.exports = class Square extends newSquare {
-
-charPrint (c) {
-	if (c === undefined){
-	this.print();
-	}
-	else {
-		for (let i = 0; i < this.width; i++){
-			console.log('C'.repeat(this.height))
-		}
-	}
-}
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.width; i++) {
+        console.log('C'.repeat(this.height));
+      }
+    }
+  }
 };
