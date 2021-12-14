6a068d7245fd4580a274a43896c20593c9a685ee
root <3290@holbertonschool.com>
2021-12-14 09:34:37 -0800

    fix100semistandar

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-let lista = require('./100-data').list;
+const lista = require('./100-data').list;
 
 const arrai2 = lista.map((x, index) => x * index);
 
 console.log(lista);
 console.log(arrai2);
-
