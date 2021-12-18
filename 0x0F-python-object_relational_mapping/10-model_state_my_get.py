664c81a1466abfa7b9276fc683454b487261637b
root <3290@holbertonschool.com>
2021-12-18 10:44:12 -0800

    fin10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+10. Get a state
+"""
+from sqlalchemy import Integer, String, Column
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+from model_state import Base, State
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base = declarative_base()
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    sessions = Session()
+    argumento = argv[4]
+    objectone = sessions.query(State).filter_by(name=argumento).first()
+    if objectone is not None:
+        print(objectone.id)
+    else:
+        print('Not Found')
+    sessions.close()
