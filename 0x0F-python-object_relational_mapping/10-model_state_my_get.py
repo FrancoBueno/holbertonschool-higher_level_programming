006dc374064f962aeed728d27150e7375b215eae
root <3290@holbertonschool.com>
2021-12-18 10:48:59 -0800

    11

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -13,8 +13,6 @@ if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
-    Base = declarative_base()
-
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     sessions = Session()
