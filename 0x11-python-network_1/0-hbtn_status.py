f75492c018517d3f1d0a2ef05483a2f6910867b1
root <3290@holbertonschool.com>
2022-01-09 11:07:37 -0800

    fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 # What's my status? #0
 
-import urllib.request
+import urllib.request as request
 if __name__ == '__main__':
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    with request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(html)))
