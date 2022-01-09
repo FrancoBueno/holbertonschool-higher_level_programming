99a1535b04b2c0081c7075faa5b73d36ae6b80c0
root <3290@holbertonschool.com>
2022-01-09 10:06:08 -0800

    fix5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #POST parameters
-curl -sX "$1" POST -d 'email=test@gmail.com' -d 'subject=I will always be here for PLD'
+curl -sX POST -d '$1' 'email=test@gmail.com' -d 'subject=I will always be here for PLD'
