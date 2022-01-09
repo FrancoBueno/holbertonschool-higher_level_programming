dcaa8abbdc7b8051849955455be8632316cb56d7
root <3290@holbertonschool.com>
2022-01-09 11:27:21 -0800

    newfix

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import urllib.request as request
-from sys import argv
 
 if __name__ == "__main__":
+    import urllib.request as request
+    from sys import argv
     with request.urlopen(argv[1]) as r:
         print(r.headers.get('X-Request-Id'))
