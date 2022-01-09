24a40d3032e9ed0a18939a58d48160faad9a1f60
root <3290@holbertonschool.com>
2022-01-09 11:01:52 -0800

    fin 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#What's my status? #0
+
+import urllib.request
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("Body response:")
+        print("- type: {}".format(type(html)))
+        print("- content: {}".format(html))
+        print("- utf8 content: {}".format(html.decode('utf-8')))
