81464916aeb9ffd5b27d6e3d9a28149cf65f8973
root <3290@holbertonschool.com>
2022-02-01 16:15:17 +0000

    fixsemi

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,8 +3,8 @@ const request = require('request');
 const url = process.argv[2];
 request(url, function (err, response) {
   if (err) {
-	  console.log(err);
-  }	else {
-	  console.log('code: ' + response.statusCode);
+    console.log(err);
+  } else {
+    console.log('code: ' + response.statusCode);
   }
 });
