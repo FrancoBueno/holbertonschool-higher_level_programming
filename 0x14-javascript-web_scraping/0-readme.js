386363b1cd2b1081e8c641007f56e43b66b0fdde
root <3290@holbertonschool.com>
2022-02-01 15:40:39 +0000

    0-Task

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
-
-const fs = require("fs");
+const fs = require('fs');
 const file = process.argv[2];
 
-fs.readFile(file, 'utf-8', function(err, data){
-	if (err) {
-		console.log(err);
-}
-	else{
-		console.log(data);
-}
+fs.readFile(file, 'utf-8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 });
