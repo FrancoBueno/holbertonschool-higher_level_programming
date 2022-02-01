67e5f11775a8e9e2ccaa3a4fa82cdc5f9da90674
FrancoBuenoHolberton <3290@holbertonschool.com>
2022-02-01 12:54:03 -0300

    ¨P

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const file = process.argv[2];
 const text = process.argv[3];
-fs.write(file, 'utf-8', text, (err) =>{
+fs.write(file, text, 'utf-8', (err) =>{
     if (err){
         console.log(err);
     }
